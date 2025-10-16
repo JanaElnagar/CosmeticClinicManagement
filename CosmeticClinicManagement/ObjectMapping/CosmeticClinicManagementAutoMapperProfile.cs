2a43fd05df0e64b1806e8a9eb463371a17e62d2d
JanaElnagar <janaelnagar18@gmail.com>
2025-10-16 12:41:33 +0300

    Initial Solution

---
+++ CosmeticClinicManagement/ObjectMapping/CosmeticClinicManagementAutoMapperProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace CosmeticClinicManagement.ObjectMapping;
+
+public class CosmeticClinicManagementAutoMapperProfile : Profile
+{
+    public CosmeticClinicManagementAutoMapperProfile()
+    {
+        /* Create your AutoMapper object mappings here */
+    }
+}
