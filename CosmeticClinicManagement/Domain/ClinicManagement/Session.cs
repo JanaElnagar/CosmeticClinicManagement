0dde2d3885c2ddbe91d68d062f06ba0b5033acd8
Abdelrahman <abdulrahmanaladdin1@gmail.com>
2025-10-21 12:50:57 +0300

    checkpoint

---
+++ CosmeticClinicManagement/Domain/ClinicManagement/Session.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace CosmeticClinicManagement.Domain.ClinicManagement
+{
+    public class Session : Entity<Guid>
+    {
+        public DateTime SessionDate { get; private set; }
+        public IEnumerable<UsedMaterial> UsedMaterials { get; private set; }
+        public IEnumerable<string> Notes { get; private set; }
+    }
+}
---
+++ CosmeticClinicManagement/Domain/ClinicManagement/UsedMaterial.cs
@@ -0,0 +1,15 @@
+﻿using Volo.Abp.Domain.Values;
+
+namespace CosmeticClinicManagement.Domain.ClinicManagement
+{
+    public class UsedMaterial : ValueObject
+    {
+        public Guid RawMaterialId { get; private set; }
+        public decimal Quantity { get; private set; }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            return [ RawMaterialId, Quantity ];
+        }
+    }
+}
