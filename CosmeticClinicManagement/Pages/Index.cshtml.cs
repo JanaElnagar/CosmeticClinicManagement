b72e347f6e435b3640a79901c9fa4e1c755f5f10
JanaElnagar <janaelnagar18@gmail.com>
2025-10-29 12:05:37 +0300

    fix null sessions exception

--- CosmeticClinicManagement/Pages/Index.cshtml.cs
+++ CosmeticClinicManagement/Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-//<<<<<<< HEAD
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,22 +13,3 @@ namespace CosmeticClinicManagement.Pages
         }
     }
 }
-//=======
-//﻿using Microsoft.AspNetCore.Authorization;
-//using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
-//using Volo.Abp.Users;
-
-//namespace CosmeticClinicManagement.Pages;
-
-//[Authorize]
-//public class IndexModel : AbpPageModel
-//{
-//    public string PartialViewName { get; set; }
-//    public string UserName { get; set; }
-//    public void OnGet()
-//    {
-//        PartialViewName = "_DoctorDashboardPartial";
-//        UserName = $"{CurrentUser.Name} {CurrentUser.SurName}";
-//    }
-//}
-//>>>>>>> 438533152f0871d479d0df826e57662b41b7535a
